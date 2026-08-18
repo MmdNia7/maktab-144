39bbd5b00384a5171ab0437d23f5091036dc63cb
Mohammad Mahdi <mh.roshan1383@gmail.com>
2026-08-18 06:49:53 -0700

    update-main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-#2
-def countdown (n):
-    for i in range(n,0,-1):
-        yield i
-print(list(countdown(5)))        
-
-double = (i**2 for i in range(1,10+1))
-print(list(double))
-print("-"*20)
-def dob(n):
-    for i in range(1,n+1):
-        yield i ** 2
-print(list(dob(10)))        
+print("Hello")
